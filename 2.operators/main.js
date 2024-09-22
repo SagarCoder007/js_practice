9519b9531c778947b47cfc5f0e4989dc1e95caa6
SagarCoder007 <sagarvarma10001@gmail.com>
2024-09-22 12:38:53 +0530

    operators done

---
+++ 2.operators/main.js
@@ -0,0 +1,29 @@
+
+// operators
+let a=10
+let b=40
+
+console.log( "a + b =",a+b)
+console.log( "a-- =",a--)   //10
+console.log( "--a =",--a)    //8
+console.log( "a++ =",a++)    //8
+console.log( "++a =",++a)    //10
+
+//  comparison operator 
+
+console.log("10 != 40 ", a != b)    
+console.log("10 == 40 ", a == b)    
+console.log("10 !== 40 ", a !== b)
+console.log("10 === 40 ", a === b)
+console.log("10 >= 40 ", a >= b)
+console.log("10 <= 40 ", a <= b)
+
+// logical operator  
+
+let u = 70
+let g = 90
+
+let con1 = u <= g
+let con2 = g === 90
+
+console.log("con1 && con2", con1 &&  con2)
