d09701b197ef1e975f71a35b3b760f575a4f1fbb
SagarCoder007 <sagarvarma10001@gmail.com>
2024-10-10 15:31:16 +0530

    basic concepts done

---
+++ 4.Basic_operations_js/main.js
@@ -0,0 +1,47 @@
+//   var ans=confirm("are you an adult ??")
+//   console.log(ans)
+
+//   var detail=prompt("enter your name here ?")
+//   console.log(detail)
+
+// var a=1
+
+// while(a <= 10){
+//     console.log(a)
+//     a++
+// }
+
+
+// var a =10
+// var b ="10"
+
+// if(a===b){
+//     console.log("sahi hai")
+// }
+// else{
+//     console.log("galat hai")
+// }
+
+// function hey(){
+//     console.log("hey i am sagar")
+// }
+// hey()
+
+// function hello(a){
+//     console.log("hey i am sagar",a)
+// }
+// hello("i live in mumbai")
+
+// function back(){
+//     console.log("hello")
+//     return 40
+// }
+//  var b=back()
+//  console.log(b)
+
+//  back()
+
+// var abc = ()=>{
+//     console.log('hello im back')
+// }
+// abc()
